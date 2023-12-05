a61e359bfe3ff64044284ba71ecb1069cb95abf1
adithyakb10 <132006470+adithyakb10@users.noreply.github.com>
2023-12-05 07:25:57 +0530

    add form and home page with stylesheets and images

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
 //jshint esversion:6
-require ('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require('ejs');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require('mongoose-findorcreate');
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 const path = require("path");
 
-const app =  express();
+const app = express();
 
 app.use(express.static("public"));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(express.static("public/css"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(session({
-    secret: "Our little secret.",
+app.use(
+  session({
+    secret: "secret.",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewURLParser: true});
-// mongoose.set("useCreateIndex", true);
+//Connecting to db
+mongoose.connect("mongodb://localhost:2717/userDB", { useNewURLParser: true });
 
-const userSchema = new mongoose.Schema ({
-    email: String,
-    password: String,
-    googleID: String,
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  googleID: String,
+  firstname: String,
+  lastname: String,
+  state: String,
+  district: String,
+  subject: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -45,81 +54,122 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-      cb(null, { id: user.id, username: user.username});
-    });
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username });
   });
-  
-passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, user);
-    });
-  });
-
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/home",
-    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleID: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
-
-
-app.get("/login", (req,res)=>{
-    res.render("login.ejs")
 });
 
-app.route('/auth/google')
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
-  .get(passport.authenticate('google', {
+//Configuring OAuth to allow users to sign in using their google account
 
-    scope: ['profile']
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/home",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleID: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
-  }));
+//Rendering login page
 
-app.get("/auth/google/home", 
-  passport.authenticate('google', { failureRedirect:"/login" }),
-  function(req, res) {
+app.get("/", (req, res) => {
+  res.render("login.ejs");
+});
+
+app
+  .route("/auth/google")
+
+  .get(
+    passport.authenticate("google", {
+      scope: ["profile"],
+    })
+  );
+
+app.get(
+  "/auth/google/home",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
     // Successful authentication, redirect home.
     res.redirect("/home");
-  });
+  }
+);
 
+//Rendering Register Page
 
-app.get("/register", (req,res)=>{
-    res.render("register.ejs")
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
 });
 
-app.get("/home", (req,res)=>{
-    res.render("home.ejs")
+//Rendering Home Page
+
+app.get("/home", (req, res) => {
+  res.render("home.ejs");
 });
 
+//Rendering Form Page
 
-app.post("/register", function(req, res){
-    User.register({username: req.body.username}, req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            res.redirect("/register");
-        } else {
-            passport.authenticate("local")(req, res, function(){
-                res.redirect("/home");
-            })
-        }
-    });
-
-})
-
-app.post('/login', passport.authenticate('local', {
-    successRedirect: '/home',
-    failureRedirect: '/login'
-  }));
-
-app.listen(3000, function(){
-    console.log("Server running on port 3000");
+app.get("/form", (req, res) => {
+  res.render("form.ejs");
 });
 
+//Handling Post Requests
+
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/home");
+        });
+      }
+    }
+  );
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+  })
+);
+
+//Rendering form page
+app.get("/form", function (req, res) {
+  res.render("form.ejs");
+});
+
+//Registering data to db
+// app.post("/form-submit", function (req, res) {
+//   User.findOneAndUpdate({email:},{
+//     firstname: req.body.firstname,
+//     lastname: req.body.lastname,
+//     state: req.body.state,
+//     district: req.body.district,
+//   });
+// });
+
+//Collecting data from the user
+
+app.listen(3000, function () {
+  console.log("Server running on port 3000");
+});
